eef61dee524af3b0c0beb2855d71d8d69a5b8005
vircoin <30941973+vircoin@users.noreply.github.com>
2017-08-13 04:49:03 +0700

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "vircoin")
+set(CN_CURRENCY_DISPLAY_NAME "vircoin")
+set(CN_CURRENCY_TICKER "vircoin")
